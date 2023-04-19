75730feca8a799c112efceafcd759ddad44490ac
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2023-04-19 17:22:51 +0700

    fix error migration user two factor

--- config/fortify.php
+++ config/fortify.php
@@ -137,10 +137,10 @@ return [
         // Features::emailVerification(),
         // Features::updateProfileInformation(),
         // Features::updatePasswords(),
-        // Features::twoFactorAuthentication([
-        //     'confirm' => true,
-        //     'confirmPassword' => true,
-        // ]),
+        Features::twoFactorAuthentication([
+            'confirm' => true,
+            'confirmPassword' => true,
+        ]),
     ],
 
 ];
